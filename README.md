b8e81b01ea160a2115b64bdc025dbf84ed945387
bearddan2000 <67988550+bearddan2000@users.noreply.github.com>
2020-11-27 21:55:16 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# saltstack-cli-csharp-linq-simple
+
+## Description
+A c# linq demo that uses a primative data
+type collection as input.
+Built by salt.
+
+## Tech stack
+- bash
+- c#
+  - Framework 4.7
+- mono 6.8
+- salt
+
+## Docker stack
+- docker-cli
+- ubuntu:20.04
+
+## Requirements
+Docker desktop must be installed and the application
+being called must be linux compatiple.
+
+## To run
+`sudo ./install.sh -u`
+
+## To stop (optional)
+`sudo ./install.sh -d`
+
+## To see help
+`sudo ./install.sh -h`
---
+++ bin/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MyAppCshrp
+{
+    class MainClass
+    {
+        private static void print(IEnumerable<int> result, String query)
+        {
+            Console.WriteLine("[QUERY] {0}", query);
+            foreach (int i in result)
+                Console.WriteLine("[OUTPUT] {0}", i);
+        }
+        private static void greaterThan(List<int> list)
+        {
+            var result = (from x in list where x > 1 select x);
+            print(result, "SELECT * FROM list WHERE x > 1;");
+        }
+        private static void lessThan(List<int> list)
+        {
+            var result = (from x in list where x < 2 select x);
+            print(result, "SELECT * FROM list WHERE x < 2;");
+        }
+        private static void equals(List<int> list)
+        {
+            var result = (from x in list where x == 1 select x);
+            print(result, "SELECT * FROM list WHERE x = 1;");
+        }
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 0, 1, 2 };
+            Console.WriteLine("[INPUT] {0}", String.Join(", ", list.ToArray()));
+            greaterThan(list);
+            lessThan(list);
+            equals(list);
+        }
+    }
+}
